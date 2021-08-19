6a964ee02619dee83e528a667370a4059520efd8
Boris Hristov <bhristov@esri.com>
2021-08-19 16:22:51 -0700

    Create runtimecore branch
    Add PROJ.lua
    Add conanfile_rtc.py

---
+++ conanfile_rtc.py
@@ -0,0 +1,23 @@
+from conans import ConanFile
+
+
+class PROJConan(ConanFile):
+    name = "PROJ"
+    version = "v8.1.0"
+    url = "https://github.com/Esri/PROJ/tree/runtimecore"
+    license = "https://github.com/OSGeo/PROJ/blob/master/COPYING"
+    description = "PROJ"
+
+    # RTC specific triple
+    settings = "platform_architecture_target"
+
+    def package(self):
+        base = self.source_folder + "src/"
+        relative = "3rdparty/PROJ/"
+
+        # headers
+        self.copy("*.h*", src=base, dst=relative)
+ 
+        # libraries
+        output = "output/" + str(self.settings.platform_architecture_target) + "/staticlib"
+        self.copy("*" + self.name + "*", src=base + "../../" + output, dst=output)
