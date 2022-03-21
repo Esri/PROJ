4058197a814cbb5b75e0ba6be4c1c02a088d056a
Christian Venegas <cvenegas@esri.com>
2022-03-21 09:37:50 -0700

    arch: Fix casing for RTC files (#4)
    Boris Hristov <BHristov@esri.com>

--- conanfile_rtc.py
+++ conanfile_rtc.py
@@ -2,11 +2,11 @@ from conans import ConanFile
 
 
 class PROJConan(ConanFile):
-    name = "PROJ"
+    name = "proj"
     version = "v8.1.0"
     url = "https://github.com/Esri/PROJ/tree/runtimecore"
-    license = "https://github.com/OSGeo/PROJ/blob/master/COPYING"
-    description = "PROJ"
+    license = "https://github.com/Esri/PROJ/blob/runtimecore/COPYING"
+    description = "PROJ is a generic coordinate transformation software, that transforms coordinates from one coordinate reference system (CRS) to another."
 
     # RTC specific triple
     settings = "platform_architecture_target"
@@ -17,7 +17,7 @@ class PROJConan(ConanFile):
 
         # headers
         self.copy("proj.h", src=base + "src", dst=relative + "src")
- 
+
         # libraries
         output = "output/" + str(self.settings.platform_architecture_target) + "/staticlib"
         self.copy("*" + self.name + "*", src=base + "../../" + output, dst=output)
