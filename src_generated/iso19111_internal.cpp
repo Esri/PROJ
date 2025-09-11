e10ae28348060e15465fb6266b68574455447e61
Christian O. Venegas <cvenegas@esri.com>
2025-09-11 14:06:37 -0700

    Add stub files to deal with premake limitations
    Premake on windows doesn't allow two files in the same project to have
    the same name. Get around this by using stub files that have a different
    name.

---
+++ src_generated/iso19111_internal.cpp
@@ -0,0 +1,6 @@
+// stub include cpp file to workaround premake limitations
+// Two files can't have the same name in a project
+// conflicting files:
+// src/internal.cpp
+// src/iso19111/internal.cpp
+#include "../src/iso19111/internal.cpp"
